690e2d5054268107027485e677d24da5bf8beda5
C J Silverio <ceejceej@gmail.com>
2020-04-23 13:55:36 -0700

    feat: implement it
    It was simple enough not to need an argument parser or any kind
    of cli framework.
    
    Documentation in the readme.

--- README.md
+++ README.md
@@ -1,7 +1,23 @@
 # octosecret
 
-Encrypt secrets for use with Github secrets.
+Encrypt secrets using libsodium sealed boxes, via [sodiumoxide](https://docs.rs/sodiumoxide/0.2.5/sodiumoxide/). My use case is to set [Github secrets](https://developer.github.com/v3/actions/secrets/#create-or-update-a-secret-for-a-repository) in some automated workflows; you might find it similarly handy.
 
-# License
+Takes one argument: a base64-encoded public key. Reads the secret value from stdin. It sends to stdout the base64-encoded result of encrypting the input with the public key.
+
+```shell
+echo "it's a secret to everyone" | octosecret <b64-encoded-public-key> <<<
+```
+
+If you have administrative access to a github repo plus an api token handy, you can fetch its public key like this:
+
+```shell
+http GET https://api.github.com/repos/ceejbot/octosecret/actions/secrets/public-key Authorization:"token deadbeef" | jq .key
+```
+
+## Installing
+
+Downloadable binaries for darwin & amd64 are provided on the releases page.
+
+## License
 
 MIT
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,46 @@
+use sodiumoxide::crypto::box_::curve25519xsalsa20poly1305::PublicKey;
+use sodiumoxide::crypto::sealedbox;
+use std::env;
+use std::io::{Read, Write};
+
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    if args.len() < 2 {
+        eprintln!("Got {} arguments; expected 2.", args.len());
+        eprintln!("Usage: octocat base64_encoded_pub_key < secretdata");
+        ::std::process::exit(1);
+    }
+
+    let keybytes = match base64::decode(&args[1]) {
+        Ok(k) => k,
+        Err(e) => {
+            eprintln!("Failed to base64-decode the public key argument! {:?}", e);
+            ::std::process::exit(1);
+        },
+    };
+    let pubkey = match PublicKey::from_slice(&keybytes) {
+        Some(k) => k,
+        None => {
+            eprintln!("Public key input was not valid public key data!");
+            ::std::process::exit(1);
+        },
+    };
+
+    // max length of a secret is 64K. Our average secret is much smaller.
+    let mut input = Vec::with_capacity(1024);
+    let mut stdin = std::io::stdin();
+    stdin.read_to_end(&mut input).unwrap();
+
+    // for the love of God, Montresor!
+    let sealed = sealedbox::seal(&input, &pubkey);
+
+    let mut encoded = Vec::new();
+    {
+        let mut enc = base64::write::EncoderWriter::new(&mut encoded, base64::STANDARD);
+        enc.write_all(&sealed).unwrap();
+        enc.finish().unwrap();
+    }
+
+    // we know b64 is safe so we unwrap with abandon
+    println!("{}", std::str::from_utf8(&encoded).unwrap());
 }
